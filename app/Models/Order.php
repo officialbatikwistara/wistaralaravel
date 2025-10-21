2a300747514a1bf4bfa74a04f1354d6b137929b7
officialbatikwistara <official.batikwistara@gmail.com>
2025-10-21 09:08:47 +0700

    order

---
+++ app/Models/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $table = 'orders';
+
+    protected $fillable = [
+        'user_id',
+        'nama',
+        'telepon',
+        'alamat',
+        'catatan',
+        'total',
+        'tipe_order',
+        'metode_pembayaran',
+        'tanggal_ambil',
+        'status'
+    ];
+}
